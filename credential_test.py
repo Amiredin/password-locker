d1367e4c3004c9c8bbb68e0927ce4c3434668203
Amir Edin <amiredin@yahoo.com>
2019-04-06 16:16:07 +0300

    add setup unittest credentia_test.py

--- credential_test.py
+++ credential_test.py
@@ -1,4 +1,30 @@
+
 import unittest
-from credentials import Credential
+from credential import Credential
 class TestCredential(unittest.TestCase):
-    
+
+    """
+    Test class that defines test cases for the credential class behaviours,
+    the arguments help in creating test cases.
+    """
+
+    def setUp(self):
+
+        """
+        this method runs before each test case, carries the instrctions of what is to be done
+        """
+
+        self.new_password = Credential("amiresh")
+
+    def test_init(self):
+
+        """
+        used to test if the objects have been initialized properly
+        """
+
+        self.assertEqual(self.new_password.credential_detail,"amiresh")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
